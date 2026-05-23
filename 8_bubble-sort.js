a9d5acbac5a0b2248ab6a4a58fa4845a17db0a65
NikhilMandaliya <nikhilrm8923@gmail.com>
2026-05-23 14:49:35 +0530

    sort: bubble sort with comparator function

--- 8_bubble-sort.js
+++ 8_bubble-sort.js
@@ -1,8 +1,9 @@
-function bubbleSort(arr) {
+function bubbleSort(arr, comparator = (a, b) => a - b) {
   for (let i = arr.length - 1; i > 0; i--) {
     let isSwapped = false;
     for (let j = 0; j <= i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
+      const result = comparator(arr[j], arr[j + 1]);
+      if (result === true || result >= 0) {
         swap(arr, j, j + 1);
         isSwapped = true;
       }
@@ -20,3 +21,56 @@ console.log(bubbleSort([5, 6, 2, 3, 1, 4])); // [1, 2, 3, 4, 5, 6]
 console.log(bubbleSort([22, 4, 15, 3, 1, 7])); // [1, 3, 4, 7, 15, 22]
 console.log(bubbleSort([7, 1, 2, 3, 4, 5, 6])); // [1, 2, 3, 4, 5, 6, 7]
 console.log(bubbleSort([1, 15, 6, 19, 22, 24, 27, 32, 45])); // [1, 6, 15, 19, 22, 24, 32, 45]
+
+console.log(bubbleSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+console.log(bubbleSort([0, -10, 7, 4])); // [-10, 0, 4, 7]
+console.log(bubbleSort([1, 2, 3])); // [1, 2, 3]
+console.log(bubbleSort([]));
+
+const nums = [
+  4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2, 453, 546, 75, 67, 4342,
+  32,
+];
+console.log(bubbleSort(nums)); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67, 75, 232, 232, 453, 546, 4342]
+
+const kitties = ['LilBub', 'Garfield', 'Heathcliff', 'Blue', 'Grumpy'];
+
+function strComp(a, b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+  return 0;
+}
+
+console.log(bubbleSort(kitties, strComp)); // ["Blue", "Garfield", "Grumpy", "Heathcliff", "LilBub"]
+
+const moarKittyData = [
+  {
+    name: 'LilBub',
+    age: 7,
+  },
+  {
+    name: 'Garfield',
+    age: 40,
+  },
+  {
+    name: 'Heathcliff',
+    age: 45,
+  },
+  {
+    name: 'Blue',
+    age: 1,
+  },
+  {
+    name: 'Grumpy',
+    age: 6,
+  },
+];
+
+function oldestToYoungest(a, b) {
+  return b.age - a.age;
+}
+
+console.log(bubbleSort(moarKittyData, oldestToYoungest)); // sorted by age in descending order
