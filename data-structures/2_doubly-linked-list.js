dda81e03457edf0202e3bccd21402d7886e0d6b5
NikhilMandaliya <nikhilrm8923@gmail.com>
2026-09-01 23:35:51 +0530

    doubly linked list: constructor, push and pop

---
+++ data-structures/2_doubly-linked-list.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const node = new Node(val);
+    if (!this.length) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+    }
+    this.tail = node;
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.length) return undefined;
+    const popped = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return popped;
+    }
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+    this.length--;
+    popped.prev = null;
+    return popped;
+  }
+
+  log() {
+    if (!this.length)
+      return console.log('length:', this.length, 'values:', 'null');
+    let curr = this.head;
+    let result = [];
+    while (curr !== null) {
+      result.push(curr.val);
+      curr = curr.next;
+    }
+    console.log(
+      'length:',
+      this.length,
+      'values:',
+      result.join(' <-> ') + ' <-> null'
+    );
+  }
+}
